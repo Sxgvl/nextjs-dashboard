f83786dfe96e2da07114627945bfee3f1a76161c
Segal G <77511272+Sxgvl@users.noreply.github.com>
2025-01-02 11:25:49 +0100

    💄 Optimizing Fonts and Images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+// primary font
+import { Inter } from 'next/font/google';
+// secondary font
+import { Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
