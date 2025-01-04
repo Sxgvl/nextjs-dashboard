a11185dd8b02a2c4740f68f20bb7fde53e4dba74
Segal G <77511272+Sxgvl@users.noreply.github.com>
2025-01-05 00:39:17 +0100

    ✨ Adding pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,6 +13,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
     // Web API that provides utility methods for manipulating the URL query parameters.
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1'); // when the user types a new search query, you want to reset the page number to 1.
     if (term) {
       params.set('query', term);
     } else {
